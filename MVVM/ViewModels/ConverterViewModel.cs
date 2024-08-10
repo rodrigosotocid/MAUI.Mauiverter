5f45c4692f952b35b4b8c10f48259c3627f83ea7
Rodrigo Soto Cid <rodrigo.soto.web82@gmail.com>
2024-08-10 09:45:09 +0200

    Rellenando las colecciones de Unidades

--- MVVM/ViewModels/ConverterViewModel.cs
+++ MVVM/ViewModels/ConverterViewModel.cs
@@ -1,12 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
+using UnitsNet;
 
 namespace MAUI.Mauiverter.MVVM.ViewModels
 {
     public class ConverterViewModel
     {
+        public string? QuantityName { get; set; }
+        public ObservableCollection<string>? FromMeasures { get; set; }
+        public ObservableCollection<string>? ToMeasures { get; set; }
+        public string? CurrentFromMeasure { get; set; }
+        public string? CurrentToMeasure { get; set; }
+
+        public ConverterViewModel()
+        {
+            QuantityName = "Length";
+            FromMeasures = LoadMeasures();
+            ToMeasures = LoadMeasures();
+
+            CurrentFromMeasure = "Meter";
+            CurrentToMeasure = "Centimeter";
+        }
+
+        private ObservableCollection<string>? LoadMeasures()
+        {
+            var types = Quantity.Infos?
+                .FirstOrDefault(x => x.Name == QuantityName)?.UnitInfos
+                .Select(u => u.Name).ToList();
+
+            return (types is null) ? null : new ObservableCollection<string>(types);
+        }
     }
 }
